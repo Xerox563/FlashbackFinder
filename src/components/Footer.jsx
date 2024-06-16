9d6c5653ea18e7923f382ec65a94a9ad7740ad66
Amit Gangwar <gangwaramit449@gmail.com>
2024-06-16 21:51:03 +0530

    Web App For Storing the Memories

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const date = new Date();
+const year = date.getFullYear();
+
+function Footer() {
+  return <footer>{/* <p>© {year} Surajv311</p> */}</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      {/* <HighlightIcon /> */}
+      <h1>Notes_Keeper App</h1>
+    </header>
+  );
+}
+
+export default Header;
